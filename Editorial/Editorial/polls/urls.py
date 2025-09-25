66cd547db61ca7a04b4d9606fe904dd02de2f7f2
AmeLopez12 <americalopezlemus@gmail.com>
2025-09-25 09:06:20 -0600

    Volver a iniciar, salen errores

--- Editorial/Editorial/polls/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = "polls"
-
-urlpatterns = [
-    path("", views.poll_create, name="poll_create"),
-]
--- Editorial/Editorial/polls/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from .forms import PollForm
-
-def poll_create(request):
-    form = PollForm()
-    return render(request, "polls/submit.html", {"form": form})
